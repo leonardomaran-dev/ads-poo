de64c97f61ee3dfb17b88f2a45f46e9fa538e2e6
Leonardo Maran <leonardo_maran@outlook.com>
2026-08-20 18:11:08 -0300

    criação dos metodos: contrutor, getters e setters

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,14 +1,52 @@
 public class Usuario {
-    protected int id;
-    protected String avatar;
-    protected String nome;
-    protected String telefone;
-    protected String email;
+    private int id;
+    private String avatar;
+    private String nome;
+    private String telefone;
+    private String email;
 
+    //CONSTRUTOR
     public Usuario(String avatar, String nome, String telefone, String email) {
         this.avatar = avatar;
         this.nome = nome;
         this.telefone = telefone;
+        this.email = email;
+    }
+
+    //GETTERS / SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
