509e69850ccdbdd7dc50c5348d81076f5abc557a
Fernando <fernando.monteiro@sou.unifeob.edu.br>
2026-08-26 10:24:57 -0300

    criação morador

--- src/Morador.java
+++ src/Morador.java
@@ -1,3 +1,68 @@
-public class Morador {
+public class CriarMorador {
+
+    private int id;
     private String nome;
+    private int idade;
+    private String telefone;
+    private String email;
+    private String endereco;
+
+    public CriarMorador(String nome, int idade, String telefone,
+                        String email, String endereco) {
+
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
